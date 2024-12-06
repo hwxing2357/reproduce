b375f94c31ee231e1f47546ef7aee14908381f97
hwxing2357 <107153578+hwxing2357@users.noreply.github.com>
2024-12-06 11:42:24 +0000

    Add files via upload

---
+++ runner.sh
@@ -0,0 +1,1 @@
+python utils/main.py --model er --visualize-latent --dataset seq-cifar100 --load_best_args --seed 1  --buffer_size 500 --use_context --num_labels 5 --np_type npcl --forward_times_train 15 --forward_times_test 15 --kl-g 0.01 --kl-t 0.05 --kd-tr 0.1 --kd-gr 0.08 --context-batch-factor 0.125 
